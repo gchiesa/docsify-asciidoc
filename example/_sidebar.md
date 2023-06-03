9f9640aed726442035acba97712c383b89f43fb2
Giuseppe Chiesa <mail@giuseppechiesa.it>
2023-06-03 18:28:46 +0200

    add support for internal asciidoctor or downdoc + example

---
+++ example/_sidebar.md
@@ -0,0 +1,6 @@
+**HOME**
+* [docsify-asciidoc](README.adoc)
+
+**EXAMPLES**
+* [asciidoc example](chronicles-example.adoc)
+* [markdown example](markdown-example.md)
--- src/index.js
+++ src/index.js
@@ -2,25 +2,41 @@
 'use strict';
 
 const downdoc = require('downdoc')
+const turndown = require('turndown')
+
+import TurndownService from "turndown"
 
 // config options
-const asciidocToMarkdownOptions = {
+const docsifyAsciidocOptions = {
+    asciidoctorEnabled: false,
+    asciidoctorProcessor: undefined,
+    downdocAttributes: undefined,
+    ext: '.adoc',
+    debug: false,
 }
 
 // main function
-function asciidocToMarkdown(hook, vm) {
+function docsifyAsciidoc(hook, vm) {
     hook.beforeEach(function (content, next) {
         let markdown = ""
-        if (!vm.route.file.endsWith(".adoc")) {
-            console.debug("File [" + vm.route.file + "] is not asciidoc, continuing...")
+        if (!vm.route.file.endsWith(docsifyAsciidocOptions.ext)) {
+            debug("file [" + vm.route.file + "] is not asciidoc, continuing...")
+            debug("content" + content)
             return next(content)
         }
         try {
-            console.debug("Initial data below:");
-            console.debug(content);
-            markdown = downdoc(content, { attributes: { 'markdown-list-indent': 4 } })
-            console.debug("markdown converted below:")
-            console.debug(markdown)
+            let data = ""
+            debug("converting asciidoc to markdown")
+            if (docsifyAsciidocOptions.asciidoctorEnabled) {
+                let asciidoctor = (docsifyAsciidocOptions.asciidoctorProcessor !== undefined) ? docsifyAsciidocOptions.asciidoctorProcessor : Asciidoctor()
+                debug("using asciidoctor instance")
+                data = asciidoctor.convert(content)
+                debug("instantiating turndown")
+                markdown = TurndownService().turndown(data)
+            } else {
+                debug("using embedded converted")
+                markdown = downdoc(content, docsifyAsciidocOptions.downdocAttributes)
+            }
         } catch (err) {
             console.error(err);
         }
@@ -28,14 +44,18 @@ function asciidocToMarkdown(hook, vm) {
     });
 }
 
-// find customPageFooter plugin options
-window.$docsify.asciidocToMarkdown = Object.assign(
-    asciidocToMarkdownOptions,
-    window.$docsify.asciidocToMarkdown,
+function debug(msg) {
+    if (docsifyAsciidocOptions.debug) console.log('[docsifyAsciidoc] log: ' + msg);
+}
+
+// find asciidocToMarkdown plugin options
+window.$docsify.docsifyAsciidoc = Object.assign(
+    docsifyAsciidocOptions,
+    window.$docsify.docsifyAsciidoc,
 );
 
 // Set docsify plugin
 window.$docsify.plugins = [].concat(
-    asciidocToMarkdown,
+    docsifyAsciidoc,
     window.$docsify.plugins,
 );
